e1ab60d7bcfa2a25e4800796be4fdbb222d1a8e7
Glenn Mendonca <glenm.code@gmail.com>
2022-03-19 01:00:48 +0530

    Navbar CSS

--- public/JavaScript/index.js
+++ public/JavaScript/index.js
@@ -1,4 +1,18 @@
+// $(window).scroll(function () {
+//   if ($(this).scrollTop() >= 50) $("nav").addClass("main");
+//   else $("nav").removeClass("main");
+// });
+
 $(window).scroll(function () {
-  if ($(this).scrollTop() >= 50) $("nav").addClass("main");
-  else $("nav").removeClass("main");
+  var top_of_element = $(".head-nav").offset().top;
+  var bottom_of_element =
+    $(".head-nav").offset().top + $(".head-nav").outerHeight();
+  var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+  var top_of_screen = $(window).scrollTop();
+
+  if (bottom_of_screen > top_of_element && top_of_screen < bottom_of_element) {
+    $("nav").removeClass("main");
+  } else {
+    $("nav").addClass("main");
+  }
 });
