3e8a117edf57fdb2b4b461dfd4a7116f24eb62ef
Sarah Abraham <sarahabraham2014@gmail.com>
2023-06-26 20:17:24 +0530

    updated mentorship page

--- docs/JavaScript/mentorship.js
+++ docs/JavaScript/mentorship.js
@@ -8,3 +8,29 @@ $(".career-advice").hide();
 $(".placement-training").hide();
 $(".financial-training").hide();
 $(".others").hide();
+
+$(document).ready(function() {
+    $(".style_prevu_kit").click(function() {
+      var main_heading = $(this).text();
+      var lowercase_class = main_heading.toLowerCase().replace(/\s+/g, "-");
+      var demo = "." + lowercase_class;
+      var mentorshipContent = $(demo).html();
+
+
+      $(".mentor-title").text(main_heading);
+      $(".mentor-categories").html(mentorshipContent);
+
+      $(".project-mentorship").hide();
+      $(".technology").hide();
+      $(".career-advice").hide();
+      $(".placement-training").hide();
+      $(".financial-training").hide();
+      $(".others").hide();
+
+      // Show the mentor card in the corresponding section
+      $(".project-mentorship .card:contains(" + mentorName + ")").show();
+      $(".technology .card:contains(" + mentorName + ")").show();
+      $(".career-advice .card:contains(" + mentorName + ")").show();
+
+    });
+  });
